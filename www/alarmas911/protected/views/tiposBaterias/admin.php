9e2a601b2aebb9b1361a3cfa4f30dcf3d6939b58
Franco Magrini <franco.mgr@gmail.com>
2015-12-10 18:54:34 -0300

    modelo v5_5

--- www/alarmas911/protected/views/tiposBaterias/admin.php
+++ www/alarmas911/protected/views/tiposBaterias/admin.php
@@ -50,7 +50,7 @@ o <b>=</b>) al comienzo de cada uno de sus valores de búsqueda para especificar
 		'observaciones',
 		array(
 			'class'=>'CButtonColumn',
-			'template'=>'{update}'
+			'template'=>'{update}{delete}'
 		),
 	),
 )); ?>
